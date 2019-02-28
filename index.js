e2ebb136e78c5462641aa62bf94c67f6b49bc01b
Graham Hewett <hewettgraham@gmail.com>
2019-02-28 17:28:12 +0000

    changed greeting msg

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ 'hello': 'world' });
+  res.send({ 'hi': 'world' });
 });
 
 // app.get('/', (req, res) => {
