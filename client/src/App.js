3dca7332cbedbe0cb2372a42454f9e92d280e5e0
Graham Hewett <hewettgraham@gmail.com>
2019-03-01 23:00:31 +0000

    add redux and auth reducer in store

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <a
             className="Google-Oauth"
             href="/auth/google/" //proxy settings in client package.json handle this
@@ -19,7 +13,6 @@ class App extends Component {
           >
             Sign in with Google!
           </a>
-        </header>
       </div>
     );
   }
