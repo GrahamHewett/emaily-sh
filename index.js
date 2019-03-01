803833d231332b0c61592d29244f6b7808192745
Graham Hewett <hewettgraham@gmail.com>
2019-03-01 01:28:08 +0000

    refactor app add routes, services folders, begin mongoDB integration.

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
 const express = require("express");
-const passport = require("passport");
-const googleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
+require("./services/passport");
+const mongoose = require("mongoose");
 
 const app = express();
-passport.use(
-  new googleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  }, (accessToken, refreshToken, profile, done) => {
-    console.log('access token :', accessToken);
-    console.log('refresh token :', refreshToken);
-    console.log('profile :', profile);
-  })
-);
+require("./routes/authRoutes")(app);
 
-app.get("/auth/google/", passport.authenticate('google', {
-  scope: ['profile', 'email']
-}));
-
-app.get("/auth/google/callback", passport.authenticate('google', {
-  
-}));
+mongoose.connect(keys.mongoURI);
 
 app.get("/", (req, res) => {
   res.send({ YO: "world" });
