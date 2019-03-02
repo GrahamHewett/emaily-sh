308b69373c4d905da255989211f0742e26a18093
Graham Hewett <hewettgraham@gmail.com>
2019-03-02 00:43:16 +0000

    define route specific rendering using dummy routes via RRDOM.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,33 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "./App.css";
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>New Survey</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <a
-            className="Google-Oauth"
-            href="/auth/google/" //proxy settings in client package.json handle this
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Sign in with Google!
-          </a>
+        <Router>
+          <>
+            <Header />
+            <Route path="/" exact component={Landing} />
+            <Route path="/surveys" exact component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+            <a
+              className="Google-Oauth"
+              href="/auth/google/" //proxy settings in client package.json handle this
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Sign in with Google!
+            </a>
+          </>
+        </Router>
       </div>
     );
   }
